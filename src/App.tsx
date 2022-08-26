4f6ef878d32b178f20d93bc1eff7fd50bf8cec70
yuki_fujisawa <g031y148yuki@gmail.com>
2022-08-26 20:25:19 +0900

    update add count

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='p-8'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount(count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
