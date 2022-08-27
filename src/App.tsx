fdaf55a482d94c86aac81693f9422bc7cf38ddb5
yuki_fujisawa <g031y148yuki@gmail.com>
2022-08-27 12:16:54 +0900

    Add eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,38 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   const [count, setCount] = useState(0);
   const logoStyle = 'logo h-24 p-6';
 
   return (
-    <div className='App'>
+    <div className="App">
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='vite.svg' className={`${logoStyle} hover:drop-shadow-[0_0_2em_#646cffaa]`} alt='Vite logo' />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img
+            src="vite.svg"
+            className={`${logoStyle} hover:drop-shadow-[0_0_2em_#646cffaa]`}
+            alt="Vite logo"
+          />
         </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className={`${logoStyle} hover:drop-shadow-[0_0_2em_#61dafbaa]`} alt='React logo' />
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className={`${logoStyle} hover:drop-shadow-[0_0_2em_#61dafbaa]`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className='p-8'>
+      <div className="p-8">
         <button onClick={() => setCount(count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='text-[#888]'>Click on the Vite and React logos to learn more</p>
+      <p className="text-[#888]">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   );
 }
